b49fae104c4dd53939bff77fe0020fa76639116f
Kevin Mizway <mizzledragon@gmail.com>
2017-02-06 19:19:58 -0500

    Bank Account javaScript prototype transaction created. Specifications 3 and 4 have passed.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ function bankAccount(name, initialDeposit) {
     this.balance = initialDeposit;
 }
 
+bankAccount.prototype.transaction = function(deposit, withdrawal) {
+    this.balance += (deposit - withdrawal)
+    return this.balance;
+}
+
 $(function() {    
     $("#register").submit(function(event) {
         event.preventDefault();
@@ -15,5 +20,18 @@ $(function() {
 
         $(".accountName").text(newAccount.name);
         $(".balance").text(newAccount.balance.toFixed(2));
+
+        $("#transaction").submit(function(event) {
+        event.preventDefault();
+       
+        var deposit = parseFloat($("#deposit").val());
+        $("#deposit").val("")
+        var withdrawal = parseFloat($("#withdrawal").val());
+        $("#withdrawal").val("")
+
+        newAccount.transaction(deposit, withdrawal);
+
+        $(".balance").text(newAccount.balance.toFixed(2));
+        });
     });
 });
